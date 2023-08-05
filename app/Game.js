7a4c67f1d92b816b09a26336643f16adba554bae
EwelinaManiecka <ewelina.maniecka@op.pl>
2023-08-05 18:40:55 +0200

    add class Game, but don't function, all the time read console.log without that console.log doesn't exist in code

--- app/Game.js
+++ app/Game.js
@@ -1,86 +1,19 @@
-import { Quote } from "./Quote.js";
-
 class Game {
-  currentStep = 0;
-  lastStep = 7;
-
-  quotes = [
-    {
-      text: "pan tadeusz",
-      category: "Utwór literacki",
-    },
-    {
-      text: "janko muzykant",
-      category: "Utwór literacki",
-    },
-    {
-      text: "akademia pana kleksa",
-      category: "Film",
-    },
-    {
-      text: "ogniem i mieczem",
-      category: "Film",
-    },
-  ];
-
-  constructor({ lettersWrapper, categoryWrapper, wordWrapper, outputWrapper }) {
-    this.lettersWrapper = lettersWrapper;
+  constructor({ letterWrapper, categoryWrapper, outputWrapper, wordWrapper }) {
+    this.letterWrapper = letterWrapper;
     this.categoryWrapper = categoryWrapper;
-    this.wordWrapper = wordWrapper;
     this.outputWrapper = outputWrapper;
-
-    const { text, category } = this.quotes[
-      Math.floor(Math.random() * this.quotes.length)
-    ];
-    this.categoryWrapper.innerHTML = category;
-    this.quote = new Quote(text);
+    this.wordWrapper = wordWrapper;
   }
 
-  guess(letter, event) {
-    event.target.disabled = true;
-    if (this.quote.guess(letter)) {
-      this.drawQuote();
-    } else {
-      this.currentStep++;
-      document.getElementsByClassName("step")[this.currentStep].style.opacity = 1;
-      if (this.currentStep == this.lastStep) {
-        this.loosing();
-      }
-    }
-  }
-
-  drawLetters() {
+  start() {
+    // console.log("Let's play the game");
     for (let i = 0; i < 26; i++) {
       const label = (i + 10).toString(36);
       const button = document.createElement("button");
       button.innerHTML = label;
-      button.addEventListener("click", (event) => this.guess(label, event));
-      this.lettersWrapper.appendChild(button);
+      this.letterWrapper.appendChild(button);
     }
-  }
-
-  drawQuote() {
-    const content = this.quote.getContent();
-    this.wordWrapper.innerHTML = content;
-    if (!content.includes('_')) {
-        this.winning();
-    }
-  }
-
-  start() {
-    document.getElementsByClassName("step")[this.currentStep].style.opacity = 1;
-    this.drawLetters();
-    this.drawQuote();
-  }
-
-  winning() {
-    this.wordWrapper.innerHTML = "GRATULACJE! WYGRYWASZ! KONIEC GRY";
-    this.lettersWrapper.innerHTML = "";
-  }
-
-  loosing() {
-    this.wordWrapper.innerHTML = "NIESTETY! PRZEGRYWASZ! TO KONIEC GRY";
-    this.lettersWrapper.innerHTML = "";
   }
 }
 
@@ -90,5 +23,4 @@ const game = new Game({
   wordWrapper: document.getElementById("word"),
   outputWrapper: document.getElementById("output"),
 });
-
 game.start();
